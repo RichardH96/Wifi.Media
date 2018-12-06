1fcb54c80c0e8a6dcf951ab1ff8ce8dfe8af4265
Richard Humer <blabla@mail.com>
2018-12-06 18:26:20 +0100

    Changes

---
+++ TestXmlFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml;
+
+namespace TestXmlFile
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			XmlDocument xmlDocument = new XmlDocument();
+			xmlDocument.Load(@"C:\Solutions\Beispiel.xml");
+
+			XmlNodeList nodes = xmlDocument.SelectNodes("wifi/contact");
+			foreach (XmlNode node in nodes) Console.WriteLine(node.Value);
+
+		}
+	}
+}
